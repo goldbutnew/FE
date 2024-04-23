0d4dfc79f987c7a4ad9e4116f677310eb97d24bb
geumhyun lee <goldbutnew@gmail.com>
2024-04-23 10:09:39 +0900

    feat: add main link in nav

--- FRONTEND/ving/src/components/NavBar/index.tsx
+++ FRONTEND/ving/src/components/NavBar/index.tsx
@@ -1,6 +1,7 @@
 'use clinet'
 
 import React from "react";
+import Image from "next/image";
 import Link from 'next/link';
 import * as styles from './index.css'
 
@@ -9,10 +10,13 @@ export default function NavBar() {
   
   return (
     <nav className={styles.container}>
+      <Link href='/'>메인</Link>　|　 
       <Link href='/setting'>세팅</Link>　|　 
       <Link href={`/profile/${userId}`}>내 채널</Link>　|　 
       <Link href={`/studio/${userId}`}>내 스튜디오</Link>　|　 
       <Link href={`/streaming/${userId}`}>방송중인 누군가의 방</Link>　|　 
+      {/* 로그인 버튼 경로 수정 필요 */}
+      <Link href={`/streaming/${userId}`}>로그인</Link>　|　 
     </nav>
   )
 }
