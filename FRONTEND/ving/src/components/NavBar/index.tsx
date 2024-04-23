31e7641efe41f18e761420e48de0ba3b4815430c
mm9326 <mm9326@naver.com>
2024-04-23 10:12:48 +0900

    merge: develop/login

--- FRONTEND/ving/src/components/NavBar/index.tsx
+++ FRONTEND/ving/src/components/NavBar/index.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
       <Link href={`/studio/${userId}`}>내 스튜디오</Link>　|　 
       <Link href={`/streaming/${userId}`}>방송중인 누군가의 방</Link>　|　 
       {/* 로그인 버튼 경로 수정 필요 */}
-      <Link href={`/streaming/${userId}`}>로그인</Link>　|　 
+      <Link href={'/tmp'}>로그인</Link>　|　 
     </nav>
   )
 }
