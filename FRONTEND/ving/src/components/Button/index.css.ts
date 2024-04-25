7964e74b4c1ded1e1bd6559acd6a9f1f7d09b5c4
geumhyun lee <goldbutnew@gmail.com>
2024-04-25 10:18:14 +0900

    test: add test code in button component style

--- FRONTEND/ving/src/components/Button/index.css.ts
+++ FRONTEND/ving/src/components/Button/index.css.ts
@@ -44,3 +44,53 @@ export const iconButton = composeStyles(
     padding: vars.space['0.5x'],
   })
 );
+
+
+// export const defaultButton = recipe({
+//   base: {
+//     border: 'none',
+//     outline: 'none',
+//     backgroundColor: 'inherit',
+//     cursor: 'pointer',
+//     borderRadius: vars.borderRadius['1x'],
+//     // fontSize: '16px',
+//     padding: `${vars.space['1x']} ${vars.space['1x']}`,
+//     // transition: 'background-color 0.3s ease', 
+//     // ':hover': {
+//     //   backgroundColor: vars.colors.black,
+//     //   color: vars.colors.white,
+//     // }
+//     ButtonEffect
+//   },
+//   variants: {
+//     size: {
+//       small: {
+//         padding: `${vars.space['0.5x']} ${vars.space['1x']}`
+//       },
+//       // medium: {
+//       //   padding: `${vars.space['1x']} ${vars.space['2x']}` // 패딩 증가
+//       // },
+//       large: {
+//         width: '100%',
+//       }
+//     }
+//   }
+// })
+
+
+// export const iconButton = style({
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   border: `1px solid ${vars.colors.black}`,
+//   outline: 'none',
+//   backgroundColor: 'inherit',
+//   cursor: 'pointer',
+//   borderRadius: vars.borderRadius.full,
+//   padding: vars.space['0.5x'],
+//   transition: 'background-color 0.3s ease', 
+//   ':hover': {
+//     backgroundColor: vars.colors.black,
+//     color: vars.colors.white,
+//   }
+// })
