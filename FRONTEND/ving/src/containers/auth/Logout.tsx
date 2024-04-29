e59bc588aec3788050aaf526089965bf7a0d7ddb
mm9326 <mm9326@naver.com>
2024-04-29 17:41:25 +0900

    fix: logout 리다이렉트

--- FRONTEND/ving/src/containers/auth/Logout.tsx
+++ FRONTEND/ving/src/containers/auth/Logout.tsx
@@ -1,15 +1,19 @@
 'use client'
 
+import { useState, useEffect } from "react"
 import useAuthStore from "@/store/AuthStore"
+import { useRouter } from "next/navigation"
 
 import SmallButton from "@/components/Button/SmallButton"
 
 export default function Logout({ onLogoutSuccess }) {
   const { Token, logout } = useAuthStore()
+  const router = useRouter()
   
   const handleLogout = () => {
     logout()
     onLogoutSuccess()
+    router.push('/')
   }
 
   return (
