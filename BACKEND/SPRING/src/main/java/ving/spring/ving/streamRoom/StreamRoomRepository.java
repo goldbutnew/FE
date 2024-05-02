f58ac0babfaa644640e7b835f545958b0415d9dd
김상범 <oironese@naver.com>
2024-05-02 17:43:20 +0900

    fix: 몽고 DB 서버문제 해결시도

--- BACKEND/SPRING/src/main/java/ving/spring/ving/streamRoom/StreamRoomRepository.java
+++ BACKEND/SPRING/src/main/java/ving/spring/ving/streamRoom/StreamRoomRepository.java
@@ -1,12 +1,16 @@
 package ving.spring.ving.streamRoom;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import ving.spring.ving.user.UserModel;
 
 import java.util.Optional;
 
 @Repository
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.myapp.jpa.repository")
 public interface StreamRoomRepository extends JpaRepository<StreamRoomModel, Integer> {
     public Optional<StreamRoomModel> findStreamRoomModelByStreamerAndIsEnd(UserModel streamer, Boolean isEnd);
 }
