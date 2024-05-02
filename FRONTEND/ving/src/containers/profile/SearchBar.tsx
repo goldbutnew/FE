571b072e2080e2e23c6b24e3eca58387b93ab581
nyk <ryk365@naver.com>
2024-05-02 16:23:54 +0900

    style: 검색어 컴포넌트 배치

--- FRONTEND/ving/src/containers/profile/SearchBar.tsx
+++ FRONTEND/ving/src/containers/profile/SearchBar.tsx
@@ -15,37 +15,56 @@ export default function SearchBar() {
   const [message, setMessage] = useState('')
   
   const handleSearch = async () => {
-    try {
-      const response = await axios.post('/api/search/nickname', { nickname })
-      setUsers(response.data.data.users)
-      setMessage(response.data.message)
-      console.log(nickname, '성공')
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      setMessage('Failed to fetch data')
-    }
+    setUsers(users => [
+      ...users,
+      {
+        userid: 1,
+        nickname: '발루',
+        photo: 'https://picsum.photos/id/1/200/300'
+      }
+    ])
   }
+  
+  //   try {
+  //     const response = await axios.post('/api/search/nickname', { nickname })
+  //     // setUsers(response.data.data.users)
+  //     // setUsers({
+  //     //   userid: 1,
+  //     //   nickname: '발루',
+  //     //   photo: 'https://picsum.photos/id/1/200/300'
+  //     // })
+  //     setMessage(response.data.message)
+  //     console.log(nickname, '성공')
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error)
+  //     setMessage('Failed to fetch data')
+  //   }
+  // }
 
   return (
-    <div className={styles.SearchBarContainer}>
-      <div className={styles.SearchBarInputBox}>
-        <input
-          type="text"
-          placeholder="Search"
-          className={styles.input}
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-        />
-      </div>
-      <button onClick={handleSearch} className={styles.searchIcon}>
-        <FiSearch size={20}/>
-      </button>
-      {users.map(user => (
-        <div key={user.user_id}>
-          <p>{user.nickname}</p>
-          <img src={user.photo} alt={user.nickname} />
+    <div>
+      <div className={styles.SearchBarContainer}>
+        <div className={styles.SearchBarInputBox}>
+          <input
+            type="text"
+            placeholder="Search"
+            className={styles.input}
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
         </div>
-      ))}
+        <button onClick={handleSearch} className={styles.searchIcon}>
+          <FiSearch size={20}/>
+        </button>
+      </div>
+      <div>
+        {users.map(user => (
+          <div key={user.user_id}>
+            <p>{user.nickname}</p>
+            <img src={user.photo} alt={user.nickname} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
