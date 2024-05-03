f0a5bad970c13ddc06d390a7b00912cfde5617a1
geumhyun lee <goldbutnew@gmail.com>
2024-05-03 14:01:28 +0900

    feat: add axios request for get alarm

--- FRONTEND/ving/src/components/Notifer/Store.tsx
+++ FRONTEND/ving/src/components/Notifer/Store.tsx
@@ -1,5 +1,24 @@
-import create from 'zustand'
+import {create} from 'zustand'
+import axios from 'axios'
 
-const useNotiferStore = create((get) => {
+const useNotiferStore = create((set, get) => ({
 
-})
+  myAlarm: [],
+
+  getAlarm: async () => {
+    const token = localStorage.getItem('acessToken')
+    try {
+      const response = await axios.get(`stream/getAlarm`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      console.log('요청 성공', response.data)
+      set({ myAlarm: response.data })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+}))
+
+export default useNotiferStore
