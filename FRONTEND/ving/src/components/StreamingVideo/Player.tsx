8c08d919e29c4a86af610e550eedaac8df176040
mm9326 <mm9326@naver.com>
2024-05-03 17:32:27 +0900

    feat: 임시 화질 변환 완료

--- FRONTEND/ving/src/components/StreamingVideo/Player.tsx
+++ FRONTEND/ving/src/components/StreamingVideo/Player.tsx
@@ -1,17 +1,36 @@
+
 'use client';
- 
-import type { PlayerProps } from 'next-video';
+
+import React, { useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
- 
+import { PlayerProps } from 'next-video';
+import styles from './test.css';
+
 export default function Player(props: PlayerProps) {
-  let { asset, src, poster, blurDataURL, thumbnailTime, ...rest } = props;
-  let config = { file: { attributes: { poster } } };
- 
-  return <ReactPlayer
-    url={src}
-    config={config}
-    width="100%"
-    height="100%"
-    {...rest}
-  />;
+  const { src, ...rest } = props;
+  const playerRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    setPlaying(!playing); // 재생 상태 토글
+  };
+
+  return (
+    <div style={styles.playerWrapper}>
+      <ReactPlayer
+        ref={playerRef}
+        style={styles.reactPlayer}
+        url={src}
+        playing={playing} // 재생 상태
+        width="100%"
+        height="100%"
+        {...rest}
+      />
+      <div style={styles.customControls}>
+        <button style={styles.button} onClick={handlePlayPause}>
+          {playing ? 'Pause' : 'Play'} {/* 버튼 라벨 동적 변경 */}
+        </button>
+      </div>
+    </div>
+  );
 }
