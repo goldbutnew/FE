4373fd7230db4ea387c2b27d68d4238c9e2e52db
geumhyun lee <goldbutnew@gmail.com>
2024-05-03 14:13:59 +0900

    fix: accesstoken 오타 수정

--- FRONTEND/ving/src/components/Notifer/Store.tsx
+++ FRONTEND/ving/src/components/Notifer/Store.tsx
@@ -2,23 +2,26 @@ import {create} from 'zustand'
 import axios from 'axios'
 
 const useNotiferStore = create((set, get) => ({
-
   myAlarm: [],
 
   getAlarm: async () => {
-    const token = localStorage.getItem('acessToken')
+    const token = localStorage.getItem('accessToken'); // 오타 수정
+    if (!token) {
+      console.log("토큰이 없습니다.");
+      return;
+    }
     try {
-      const response = await axios.get(`stream/getAlarm`, {
+      const response = await axios.get(`http://localhost:8080/api/stream/getAlarm`, { // baseURL 확인
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      })
-      console.log('요청 성공', response.data)
-      set({ myAlarm: response.data })
+      });
+      console.log('요청 성공', response.data);
+      set({ myAlarm: response.data.alarms });
     } catch (error) {
-      console.log(error)
+      console.error('알림 데이터 요청 실패:', error);
     }
   },
-}))
+}));
 
 export default useNotiferStore
