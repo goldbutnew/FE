886be5451137f1bc70fe2c0a5e670e689998e0ae
mm9326 <mm9326@naver.com>
2024-05-07 13:09:19 +0900

    feat: mainstore 기초 작성 및 스트리밍 axios 테스트 코드 작성

--- FRONTEND/ving/src/containers/tmp/index.tsx
+++ FRONTEND/ving/src/containers/tmp/index.tsx
@@ -1,12 +1,26 @@
-'use clinet'
+'use client'
 
-import React from "react";
+import React, { useRef, useEffect } from 'react'
 import ChatTest from "./ChatTest";
+import VideoPlayer from "@/components/StreamingVideo/Player"
+
+import useStreamingStore from "@/store/StreamingStore";
 
 export default function tmp() {
+  const { closePort } = useStreamingStore()
+
+  useEffect (() => {
+    closePort('15')
+    return () => {
+      // closePort()
+    }
+  }, [])
+
+
   return (
     <div>
       <ChatTest />
+      <VideoPlayer />
     </div>
   )
 }
