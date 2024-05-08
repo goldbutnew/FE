b339fe33014a43516e12f300b5c26ba773d93ce3
김상범 <oironese@naver.com>
2024-05-08 09:07:25 +0900

    feat: 웹소켓 뉴스피드 기능 제작

--- FRONTEND/ving/src/components/NewsFeed/index.tsx
+++ FRONTEND/ving/src/components/NewsFeed/index.tsx
@@ -11,81 +11,95 @@ export default function NewsFeed() {
   const [stompClient, setStompClient] = useState(null);
   const [connected, setConnected] = useState(false);
 
-  const roomId = "a2FueWV3ZXN0";
+  const roomId = "ZGhhYWtzbHFrc2FwZ2hh";
 
   useEffect(() => {
     const sock = new SockJS('http://localhost:8080/ws');
     const stompClient = Stomp.over(sock);
 
-    stompClient.connect({}, frame => {
-      console.log('Connected: ' + frame);
+    // stompClient.connect({}, frame => {
+    //   console.log('Connected: ' + frame);
 
-      stompClient.subscribe('/topic/newsFeed', message => {
-        const newEvent = JSON.parse(message.body);
-        setEvents(prevEvents => [...prevEvents, newEvent]);
-      });
-    });
+    //   stompClient.subscribe('/topic/newsFeed', message => {
+    //     const newEvent = JSON.parse(message.body);
+    //     setEvents(prevEvents => [...prevEvents, newEvent]);
+    //   });
+    // });
 
-    return () => {
-      if (stompClient) {
-        stompClient.disconnect(() => {
-          console.log('Disconnected');
-        });
-      }
-    };
+    // return () => {
+    //   if (stompClient) {
+    //     stompClient.disconnect(() => {
+    //       console.log('Disconnected');
+    //     });
+    //   }
+    // };
   }, []);
 
-  // const onMessageReceived = (msg) => {
-  //   const newMessage = JSON.parse(msg.body);
-  //   console.log(newMessage);
-  // };
+  const onMessageReceived = (msg) => {
+    const newMessage = JSON.parse(msg.body);
+    console.log(newMessage);
+  };
 
-  // const connect = () => {
-  //   console.log("WebSocket 연결 시도 중...");
-  //   const client = Stomp.over(() => new SockJS('http://localhost:8080/ws'));
+  const connect = () => {
+    console.log("WebSocket 연결 시도 중...");
+    const client = Stomp.over(() => new SockJS('http://localhost:8080/ws'));
 
-  //   client.reconnect_delay = 5000;
-  //   client.debug = function(str) {
-  //     console.log('STOMP Debug:', str);
-  //   };
+    client.reconnect_delay = 5000;
+    client.debug = function(str) {
+      console.log('STOMP Debug:', str);
+    };
 
-  //   client.onConnect = () => {
-  //     console.log("연결 완료");
-  //     setConnected(true);
-  //     client.subscribe(`/sub/channel/${roomId}`, onMessageReceived, {
-  //       id: `sub-${roomId}`,
-  //       ack: 'client'
-  //     });
-  //   };
+    client.onConnect = () => {
+      console.log("연결 완료");
+      setConnected(true);
+      client.subscribe(`/sub/streamer/${roomId}`, onMessageReceived, {
+        id: `sub-${roomId}`,
+        ack: 'client'
+      });
+    };
 
-  //   client.onDisconnect = () => {
-  //     console.log("WebSocket 연결 해제 완료");
-  //     setConnected(false);
-  //   };
+    client.onDisconnect = () => {
+      console.log("WebSocket 연결 해제 완료");
+      setConnected(false);
+    };
 
-  //   client.activate();
-  //   setStompClient(client);
-  // };
+    client.activate();
+    setStompClient(client);
+  };
   
-  // useEffect(() => {
-  //   connect();
-  //   return () => {
-  //     if (stompClient) {
-  //       console.log("WebSocket 연결 해제 시도 중...");
-  //       stompClient.deactivate();
-  //     }
-  //   }
-  // }, []);
+  useEffect(() => {
+    connect();
+    return () => {
+      if (stompClient) {
+        console.log("WebSocket 연결 해제 시도 중...");
+        stompClient.deactivate();
+      }
+    }
+  }, []);
 
   return (
-    <div className={styles.newsFeedContainer}>
-      <div className={styles.title}>
+    // <div className={styles.newsFeedContainer}>
+    //   <div className={styles.title}>
+    //     뉴스피드
+    //   </div>
+    //   <hr className={line} />
+    //   <div className={styles.newsFeedContent}>
+    //     {events.map((event, index) => (
+    //       <div key={index} className={styles.newfeedItem}>
+    //         {event.isDonation === 0 && `🎉 ${event.user} 님이 팔로우했습니다.`}
+    //         {event.isDonation !== 0 && `🍫 ${event.user} 님이 ${event.choco} 초코를 후원했습니다.`}
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
+    <div >
+      <div>
         뉴스피드
       </div>
       <hr className={line} />
-      <div className={styles.newsFeedContent}>
+      <div >
         {events.map((event, index) => (
-          <div key={index} className={styles.newfeedItem}>
+          <div key={index} >
             {event.isDonation === 0 && `🎉 ${event.user} 님이 팔로우했습니다.`}
             {event.isDonation !== 0 && `🍫 ${event.user} 님이 ${event.choco} 초코를 후원했습니다.`}
           </div>
