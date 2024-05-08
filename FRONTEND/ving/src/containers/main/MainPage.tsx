b33ff686f1a60cd8900616ac86717634f45a0e6d
mm9326 <mm9326@naver.com>
2024-05-08 11:55:12 +0900

    fix: 리눅스 설정오류 해결

--- FRONTEND/ving/src/containers/main/MainPage.tsx
+++ FRONTEND/ving/src/containers/main/MainPage.tsx
@@ -21,7 +21,7 @@ export default function MainPage() {
       <h3>메인 페이지</h3>
       <h3>{userData.nickname} 계정으로 로그인되었습니다.</h3>
 
-      <div>//////// 테스트 페이지로 이동////////</div>
+      <div>~~~~~~~~~~~~~~~테스트 페이지로 이동~~~~~~~~~~~~~</div>
       <Link href={`/tmp`}>test</Link>
 
       <div>
