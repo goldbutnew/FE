7570feb01a485b558851d4c33c495851714e20c0
geumhyun lee <goldbutnew@gmail.com>
2024-05-08 11:03:22 +0900

    fix: all

--- FRONTEND/ving/src/components/Container/index.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css'
-import { vars } from '@/styles/vars.css'
-
-export const container = style({
-  flexGrow: 1,
-  padding: `${vars.space['8x']} ${vars.space['2x']} 0 ${vars.space['2x']}`,
-})
--- FRONTEND/ving/src/components/StreamingVideo/index.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from '@vanilla-extract/css'
-
-export const videoResize = style({
-  width: "100%",
-  aspectRatio: "16/9",
-})
