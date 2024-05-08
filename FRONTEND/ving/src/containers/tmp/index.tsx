90d378ba61a4fc856d750b36b8e0ed56709225ce
mm9326 <mm9326@naver.com>
2024-05-08 17:56:07 +0900

    feat: 음성/영상 별도 조절 테스트 코드 작성 & 화질 변경 s3 요청 성공 & videojs 컨트롤러 커스터마이징

--- FRONTEND/ving/src/containers/tmp/index.tsx
+++ FRONTEND/ving/src/containers/tmp/index.tsx
@@ -1,26 +1,72 @@
 'use client'
 
-import React, { useRef, useEffect } from 'react'
-import ChatTest from "./ChatTest";
-import VideoPlayer from "@/components/StreamingVideo/Player"
-
-import useStreamingStore from '@/store/StreamingStore'
+import { useEffect, useRef, useState } from 'react'
+import Hls from 'hls.js'
 
 export default function Tmp() {
-  const { closePort } = useStreamingStore()
+  const videoRef = useRef(null)
+  const audioRef = useRef(null)
+  const [isPlaying, setIsPlaying] = useState(false)
 
-  useEffect (() => {
-    closePort('kanyewest')
+  const syncPlayPause = () => {
+    const videoElement = videoRef.current
+    const audioElement = audioRef.current
+    if (!videoElement || !audioElement) return
+
+    if (isPlaying) {
+      videoElement.play()
+      audioElement.play()
+    } else {
+      videoElement.pause()
+      audioElement.pause()
+    }
+  }
+
+  useEffect(() => {
+    syncPlayPause()
+  }, [isPlaying])
+
+  useEffect(() => {
+    const videoElement = videoRef.current
+    const audioElement = audioRef.current
+
+    const setupHls = (element, src) => {
+      if (Hls.isSupported() && element) {
+        const hls = new Hls()
+        hls.loadSource(src)
+        hls.attachMedia(element)
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          element.play()
+        })
+        return hls
+      }
+    }
+
+    const hlsVideo = setupHls(videoElement, 'https://vingving.s3.ap-northeast-2.amazonaws.com/256/anjdidhodkseho.m3u8')
+    const hlsAudio = setupHls(audioElement, 'https://vingving.s3.ap-northeast-2.amazonaws.com/720/anjdidhodkseho.m3u8')
+
     return () => {
-      // closePort()
+      if (hlsVideo) hlsVideo.destroy()
+      if (hlsAudio) hlsAudio.destroy()
     }
   }, [])
 
-
   return (
     <div>
-      <ChatTest />
-      <VideoPlayer />
+      <video 
+        ref={audioRef}
+        autoPlay={true}
+        controls={true}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
+      <video
+        ref={videoRef}
+        autoPlay={true}
+        controls={true}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
     </div>
   )
 }
