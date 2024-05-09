5a72c8b09f4b3e17ed9227f266196b2a5d04f3b8
mm9326 <mm9326@naver.com>
2024-05-09 14:23:38 +0900

    fix: gitignore 수정

---
+++ FRONTEND/ving/src/containers/studio/Store.tsx
@@ -0,0 +1,137 @@
+import { create } from 'zustand'
+import axios from '../../api/axios'
+
+const useStudioStore = create((set) => ({
+
+  startStreaming: async (formData: FormData) => {
+    console.log(formData, '방정보완성')
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.post('stream/tmpCreate', formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'multipart/form-data',
+        }
+      })
+      console.log('방송 시작 요청', response.data)
+
+    } catch (error) {
+      console.error('방송 시작 요청 실패:', error)
+    }
+  },
+
+  openPort: async () => {
+    try {
+      const response = await axios.get('media_pipeline/main')
+      console.log('포트 개방 성공', response.data)
+
+    } catch (error) {
+      console.error('포트 개방 실패:', error)
+    }
+  },
+
+  closePort: async (username) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.patch('home/delete_streaming_room/', {
+        username
+      })
+      console.log('방송 종료 요청 성공', response.data)
+
+    } catch (error) {
+      console.error('방송 종료 요청 실패:', error)
+    }
+  },
+
+  sendStreamTitle: async (roomName) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.patch('home/set_streaming_room_name/', {
+        roomName
+      })
+      console.log('제목 수정 완료', response.data)
+
+    } catch (error) {
+      console.error('제목 수정 실패:', error)
+    }
+  },
+  sendStreamThumbnail: async (thumbNail) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.patch('update_streaming_room_thumbnail/', {
+        thumbNail
+      })
+      console.log('썸네일 수정 완료', response.data)
+
+    } catch (error) {
+      console.error('썸네일 수정 실패:', error)
+    }
+  },
+
+  sendStreamLimit: async (isAdult) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.patch('home/set_streaming_room_is_adult/', {
+        isAdult
+      })
+      console.log('연령 제한 설정 완료', response.data)
+
+    } catch (error) {
+      console.error('연령 제한 설정 실패:', error)
+    }
+  },
+
+  staticData: { 
+    playCount: 0,
+    totalViewer: 0,
+    averageViewer: 0,
+    maxViewer: 0,
+  },
+  // 유저 프로필 가져오기
+  getStaticPlayCount: async (userName: string) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.get(`auth/getProfile`, {
+        params: { userName },
+      })
+      set((prev) => ({ ...prev, staticData: { ...prev.staticData, playCount: response.data } }))
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  getStaticTotalViewer: async (userName: string) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.get(`auth/getProfile`, {
+        params: { userName },
+      })
+      set((prev) => ({ ...prev, staticData: { ...prev.staticData, totalViewer: response.data } }))
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  getStaticAverageViewer: async (userName: string) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.get(`auth/getProfile`, {
+        params: { userName },
+      })
+      set((prev) => ({ ...prev, staticData: { ...prev.staticData, averageViewer: response.data } }))
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  getStaticMaxViewer: async (userName: string) => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      const response = await axios.get(`auth/getProfile`, {
+        params: { userName },
+      })
+      set((prev) => ({ ...prev, staticData: { ...prev.staticData, maxViewer: response.data } }))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}))
+
+export default useStudioStore
