193faec5230b1f4e4586b6c00a90dc5d02830684
geumhyun lee <goldbutnew@gmail.com>
2024-05-17 12:57:41 +0900

    fix: all

--- FRONTEND/ving/src/containers/studio/StartStreaming/index.tsx
+++ FRONTEND/ving/src/containers/studio/StartStreaming/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from "react";
-import Chat from "@/components/Chat";
 import StudioStreaming from "./StudioStreaming";
 import * as styles from '../index.css'
 import NewsFeed from "@/containers/studio/StartStreaming/NewsFeed";
